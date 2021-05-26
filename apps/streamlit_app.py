7ac976e7d75051b208a96a9ed23e5e453c51d387
Felipe Vasconcelos <felipedmnq@gmail.com>
2021-05-26 06:38:33 -0300

    done some more app code

--- apps/streamlit_app.py
+++ apps/streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import folium
+import json
+from folium import plugins
+from streamlit_folium import folium_static
+from PIL import Image
 
 
 st.set_page_config(layout='wide') # turns the app scream "wide" <--->
@@ -21,9 +26,22 @@ data = get_data(path)
 
 # add a filter
 #f_attributes = st.sidebar.multiselect('Enter columns', data.columns)
-f_city = st.sidebar.multiselect('Select City', data['city'].unique())
-f_pets = st.sidebar.multiselect('Pets', data['pets'].unique())
 
+image = Image.open('../images/germany_flag3.png')
+st.sidebar.image(image)
+st.sidebar.title('Germany Rental Houses App')
+st.sidebar.markdown('#### By **Felipe Demenech Vasconcelos** :sunglasses:')
+st.sidebar.header('Filters')
+f_city = st.sidebar.multiselect('Select City', data['city'].unique())
+f_pets = st.sidebar.multiselect('Pets Allowence', data['pets'].unique())
+
+a1, a2, a3 = st.beta_columns((2.6, 6, 0.1))
+b1, b2, b3 = st.beta_columns((2.9, 6, 0.1))
+#a2.markdown("<h1 style='text-align: center; color: red;'>German Rent Houses</h1>", unsafe_allow_html=True)
+image2 = Image.open('../images/german_house.jpg')
+
+b2.image(image2)
+a2.markdown('# **_Rental Houses in Germany_**')
 st.title('Data Overview')
 
 df1 = data.groupby('city')['size', 'montly_rent', 'deposit_value', 'm2_value'].mean().round(2).reset_index()
@@ -40,6 +58,8 @@ elif (f_city == []) & (f_pets != []):
 else:
     data = data
 
+
+#c1, c2 = st.beta_columns((1, 1)) # inputs like columns
 # statistic descritive
 num_att = data[['size', 'montly_rent', 'deposit_value', 'm2_value']]
 mean = pd.DataFrame(num_att.apply(np.mean))
@@ -51,6 +71,47 @@ min_ = pd.DataFrame(num_att.apply(np.min))
 df = pd.concat([max_, min_, mean, median, std], axis = 1).reset_index()
 df.columns = ['attributes', 'max_', 'min_', 'mean', 'median', 'std']
 
-st.dataframe(df)
+
 # st.dataframe(df1)
-st.dataframe(data) # display infos in the webbrowser
+st.header('Dataset')
+st.dataframe(data) # display infos in the webbrowsest
+st.header('Descriptive Caracteristics')
+st.dataframe(df)
+
+#-------------------
+# maps
+#-------------------
+
+st.markdown('## :earth_americas: Rental Houses Map')
+c1, c2 = st.beta_columns((1, 1))
+
+#c1.header('Map - 01')
+df = data.sample(10)
+
+# base map
+lat = 51.1642292
+long = 10.4541194
+
+germany = '../data/germany_cities.geo.json'
+geo_json_data = json.load(open(germany))
+
+map_b = folium.Map(location=[lat, long], zoom_start=5.5)
+
+prices = plugins.MarkerCluster().add_to(map_b)
+
+for lat, lng, i in zip(data['lat'],
+                       data['long'],
+                       list(range(data.shape[0]))):
+    label = f'{data["street"].values[i]} - Price: €{data["montly_rent"].values[i]} - {data["pets"].values[i]}'
+    label = folium.Popup(label, parse_html=True)
+    folium.Marker(
+        location=[lat, lng],
+        icon=None,
+        popup=label).add_to(prices)
+
+folium.GeoJson(geo_json_data, style_function=lambda feature: {'color': 'darkred', 'weight': 0.5, }).add_to(map_b)
+
+folium_static(map_b, width=1200, height=550)
+
+
+
